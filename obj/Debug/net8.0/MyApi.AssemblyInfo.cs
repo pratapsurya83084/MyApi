4d45bb62f079e5e7b69bc69818099a37c71b230c
Pratap Suryawanshi <pratapsuryawanshi007@gmail.com>
2026-03-27 23:50:09 +0530

    create verify-otp api with ttoken genrate then login user.

--- obj/Debug/net8.0/MyApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e513ba270465606be446b4124fcea44d36c24fca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+465a44e27118c2bcee7711598848c4a22a50270a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
